82b7d85c04c896b26599c00f37fa41af045e161f
lbstore <aciukadsiunciate2@gmail.com>
2018-03-27 23:23:23 +0300

    add ZKS

---
+++ Žmogaus ir kompiuterio sąveika/Visos 4 užduotys/4/HTML/files/p_sl_lin_s_virus__sukeltos_infekcijos/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k],l,_(m,n,o,p,q,r,s,_(),t,_(u,v,w,x,y,_(z,A,B,C),D,null,E,x,F,x,G,H,I,null,J,K,L,M,N,O,P,K),Q,_(),R,_(),S,_(T,[])),U,_(),V,_());}; 
+var b="url",c="p_sl_lin_s_virus__sukeltos_infekcijos.html",d="generationDate",e=new Date(1464192542691.06),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="NewUserName",k="NewPassword",l="page",m="packageId",n="f7ff0c999a214a178031771786bf3264",o="type",p="Axure:Page",q="name",r="Pūslėlinės virusų sukeltos infekcijos",s="notes",t="style",u="baseStyle",v="627587b6038d43cca051c114ac41ad32",w="pageAlignment",x="near",y="fill",z="fillType",A="solid",B="color",C=0xFFFFFFFF,D="image",E="imageHorizontalAlignment",F="imageVerticalAlignment",G="imageRepeat",H="auto",I="favicon",J="sketchFactor",K="0",L="colorStyle",M="appliedColor",N="fontName",O="Applied Font",P="borderWidth",Q="adaptiveStyles",R="interactionMap",S="diagram",T="objects",U="masters",V="objectPaths";
+return _creator();
+})());
